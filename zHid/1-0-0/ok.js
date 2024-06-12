a8cd8237cfaf1ba255781abbd072a8f33b248236
zzamytikk <zzamytikk@gmail.com>
2024-06-12 15:04:32 +0300

    🦾 Принцып работы

--- zHid/1-0-0/ok.js
+++ zHid/1-0-0/ok.js
@@ -2,9 +2,24 @@
     ✫ Версия 1.0.0
     © Copyright Плюшки для сайтов 2024
 */
-var zHid = {//Спрятать/Показать hidd
-  $:q => {//★ 
+var zHid = {//Спрятать/Показать hid
+  //zHid.$();//★ Спрятать/Показать hid (Запускаем)
+  $:() => {//Вешаем click
+    $('[zhid]').find('button:eq(0)').click(e => {
+      zHid.X($(e.target).closest('[zhid]'))
+    });
+  },
+  /* zHid(//Спрятать/Показать
+    Z//Путь до $('[zhid]')
+  ); */
+  X: Z => {//Спрятать/Показать
+    //console.debug('zhid="'+Z.attr('zhid')+'",', 'max-height: '+Z.css('max-height'));
     
+    if(Z.attr('zhid') != 1 && Z.css('max-height')=='100%'){//Открыто
+      Z.attr('zhid', 1);//Закрываем
+    } else {//Закрыто
+      Z.attr('zhid', 0);//Открываем
+    }
   }
 };
 
