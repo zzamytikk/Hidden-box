d11ea3922b0cee8267a300b7fcfb942342010283
zzamytikk <zzamytikk@gmail.com>
2024-12-26 09:22:35 +0200

    update

--- zHid/1-0-0/ok.js
+++ zHid/1-0-0/ok.js
@@ -6,12 +6,18 @@ var zHid = {//Спрятать/Показать hid
   //zHid.$();//★ Спрятать/Показать hid (Запускаем)
   $:() => {//Вешаем click
     $('[zhid]').find('[zhidb]:eq(0)').each((i, e) => {
-      if ($._data($(e)[0], 'events')?.click[0].namespace != 'zHid') { //Проверка ключа (Ключь не совпал! повесим обработчик)
+      //if ($._data($(e)[0], 'events')?.click[0].namespace != 'zHid') { //Проверка ключа (Ключь не совпал! повесим обработчик)
+      if(zHid.proNS(//Проверка namespace
+          e,//event
+          'zHid'//Какой ключь(namespace) ищим
+          //undefined = click
+        )//return true = Ненашли ключ, false = нашли!
+      ){
         $(e).on('click.zHid', e => {
           zHid.X($(e.target).closest('[zhid]'))
         });
       } else {
-        console.debug('Была попытка повторного запуска скрипта zHid.$();');
+        console.debug('Была попытка повторного запуска скрипта zHid.$(click.zHid);', e);
       }
     });
   
@@ -30,6 +36,29 @@ var zHid = {//Спрятать/Показать hid
     } else {//Закрыто
       Z.attr('zhid', 0);//Открываем
     }
+  },
+  /* proNS(//Проверка namespace
+      e,//event
+      'cod',//Какой ключь(namespace) ищим
+      'input'//$.on('input.cod'); undefined = click
+    );//return true = Ненашли ключ, false = нашли!
+  */
+  proNS: (e, P, c = 'click') => { //Ищим .cod, установленный: `$.on('click.cod', () => {})`
+    let d = $._data($(e)[0], 'events')?.[c];
+
+    //console.debug('Нашли запись: if(' + (d && d[0].namespace) + ').', d);
+
+    if (d && d[0].namespace) { //Нашли запись
+      return !d.filter(v => { //Проверим все
+        //console.debug('if(' + v['namespace'] + '==' + P + ') =>', v['namespace'] == P);
+
+        if (v['namespace'] == P) { //Нашли запись
+          return true //Выбераем совпадение
+        }
+      })[0]; //[0] Проверка первого совпадения
+    }
+
+    return true; //Пусто
   }
 };
 
