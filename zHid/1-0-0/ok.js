41595cf3e8c6f1d6ab42ceb6532c9eb1a1eccb46
zzamytikk <zzamytikk@gmail.com>
2025-11-16 19:36:20 +0200

    update

--- zHid/1-0-0/ok.js
+++ zHid/1-0-0/ok.js
@@ -2,33 +2,34 @@
     ✫ Версия 1.0.0
     © Copyright Плюшки для сайтов 2024
 */
-var zHid = {//Спрятать/Показать hid
+var zHid = { //Спрятать/Показать hid
   //zHid.$();//★ Спрятать/Показать hid (Запускаем)
-  $:() => {//Вешаем click
+  $: () => { //Вешаем click
     $('[zhid]').find('[zhidb]:eq(0)').each((i, e) => {
-      if(zHid.proNS(//Проверка namespace
-          e,//event
-          'zHid'//Какой ключь(namespace) ищим
+      if (zHid.proNS( //Проверка namespace
+          e, //event
+          'zHid' //Какой ключь(namespace) ищим
           //undefined = click
-        )//return true = Ненашли ключ, false = нашли!
-      ){
+        ) //return true = Ненашли ключ, false = нашли!
+      ) {
         $(e).on('click.zHid', e => {
+          //console.debug('click');
           zHid.X($(e.currentTarget).closest('[zhid]'))
         });
       } else {
-        console.debug('Была попытка повторного запуска скрипта zHid.$(click.zHid);', e);
+        //console.debug('Была попытка повторного запуска скрипта zHid.$(click.zHid);', e);
       }
     });
   },
   /* zHid(//Спрятать/Показать
     Z//Путь до $('[zhid]')
   ); */
-  X: Z => {//Спрятать/Показать
+  X: Z => { //Спрятать/Показать
     //console.debug('zhid="'+Z.attr('zhid')+'",', 'max-height: '+Z.css('max-height'));
-    //console.debug(Z, Z.attr('zhid') != 1 && Z.css('max-height')=='100%'? 'Закрываем':'Открываем');
-
+    //console.debug(Z, Z.attr('zhid') != 1 && Z.css('max-height')=='max-content'? 'Закрываем':'Открываем');
+    
     Z.attr('zhid',
-      +(Z.attr('zhid') != 1 && Z.css('max-height')=='100%')//true: Закрываем, false: Открываем
+      +(Z.attr('zhid') != 1 && Z.css('max-height')=='max-content') //true: Закрываем, false: Открываем
     );
   },
   /* proNS(//Проверка namespace
@@ -39,19 +40,19 @@ var zHid = {//Спрятать/Показать hid
   */
   proNS: (e, P, c = 'click') => { //Ищим .cod, установленный: `$.on('click.cod', () => {})`
     let d = $._data($(e)[0], 'events')?.[c];
-
+    
     //console.debug('Нашли запись: if(' + (d && d[0].namespace) + ').', d);
-
+    
     if (d && d[0].namespace) { //Нашли запись
       return !d.filter(v => { //Проверим все
         //console.debug('if(' + v['namespace'] + '==' + P + ') =>', v['namespace'] == P);
-
+        
         if (v['namespace'] == P) { //Нашли запись
           return true //Выбераем совпадение
         }
       })[0]; //[0] Проверка первого совпадения
     }
-
+    
     return true; //Пусто
   }
 };
